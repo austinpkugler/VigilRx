fbfffc29ea358fe391630fef5da9957c6014a707
austinpkugler <austinpkugler@gmail.com>
2021-06-18 09:14:34 -0700

    Initial commit

---
+++ contracts/Greeter.sol
@@ -0,0 +1,18 @@
+pragma solidity ^0.8.4;
+
+
+contract Greeter {
+    string public greeting;
+
+    constructor() {
+        greeting = 'Hello';
+    }
+
+    function setGreeting(string memory _greeting) public {
+        greeting = _greeting;
+    }
+
+    function greet() view public returns (string memory) {
+        return greeting;
+    }
+}
