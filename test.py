6dcb29cda4950a489a102e8770bf348e27941c1a
austinpkugler <austinpkugler@gmail.com>
2021-06-18 08:45:28 -0700

    Finished example contract deployment

--- test.py
+++ test.py
@@ -4,29 +4,11 @@ from web3 import Web3
 from solc import compile_standard
 
 
-compiled_sol = compile_standard({
+standard_json = {
     "language": "Solidity",
     "sources": {
         "Greeter.sol": {
-            "content": '''
-                 pragma solidity ^0.8.4;
-
-                 contract Greeter {
-                   string public greeting;
-
-                   constructor() public {
-                       greeting = 'Hello';
-                   }
-
-                   function setGreeting(string memory _greeting) public {
-                       greeting = _greeting;
-                   }
-
-                   function greet() view public returns (string memory) {
-                       return greeting;
-                   }
-                 }
-               '''
+            "content": ""
         }
     },
     "settings":
@@ -39,27 +21,33 @@ compiled_sol = compile_standard({
             }
         }
     }
-})
+}
 
-# w3 = Web3(Web3.HTTPProvider('http://localhost:8545'))
+with open('contracts/Greeter.sol') as file:
+    source_code = file.read()
 
+standard_json['sources']['Greeter.sol']['content'] = source_code
 
-# import json
+compiled_sol = compile_standard(standard_json)
 
-# # import solc
-# from solc import compile_source
-# from web3 import Web3
+w3 = Web3(Web3.HTTPProvider('http://localhost:8545'))
+w3.eth.default_account = w3.eth.accounts[0]
 
+bytecode = compiled_sol['contracts']['Greeter.sol']['Greeter']['evm']['bytecode']['object']
+abi = json.loads(compiled_sol['contracts']['Greeter.sol']['Greeter']['metadata'])['output']['abi']
 
-# with open('contracts/Prescription.sol') as file:
-#     sol = file.read()
+try:
+    Greeter = w3.eth.contract(abi=abi, bytecode=bytecode)
 
-# compiled_sol = compile_source(sol)
+    tx_hash = Greeter.constructor().transact()
+    tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
 
+    greeter = w3.eth.contract(address=tx_receipt.contractAddress, abi=abi)
+    print(greeter.functions.greet().call())
 
-# # compiled_sol = compile_standard(contracts)
-
-# # w3 = Web3(Web3.EthereumTesterProvider())
-# # w3 = Web3(Web3.HTTPProvider('http://localhost:8545'))
-# # w3.eth.default_account = w3.eth.accounts[0]
-# # bytecode = compiled_sol['contracts']['Greeter.sol']['Greeter']['evm']['bytecode']['object']
+    tx_hash = greeter.functions.setGreeting('Nihao').transact()
+    tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
+    print(greeter.functions.greet().call())
+except Exception as e:
+    print('abi:\n', abi, '\n\n')
+    print('error:\n', e, '\n\n')
