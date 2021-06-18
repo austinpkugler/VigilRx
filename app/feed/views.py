cfc954d9d7681fac808cda7b961c6a276870ee32
austinpkugler <austinpkugler@gmail.com>
2021-06-18 11:26:53 -0700

    Initial web app prototype

--- app/feed/views.py
+++ app/feed/views.py
@@ -1,2 +1,34 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+
+prescriptions = [
+    {
+        'name': 'Aspirin',
+        'ndc': '89342875',
+        'prescriber': '0x89205A3A3b2A69De6Dbf7f01ED13B2108B2c43e7',
+        'date': 'June 18, 2021'
+    },
+    {
+        'name': 'Hydrocodone',
+        'ndc': '532634634453',
+        'prescriber': '0x89205A3A3b2A69De6Dbf7f01ED13B2108B2c43e7',
+        'date': 'June 12, 2021'
+    },
+    {
+        'name': 'Fentanyl',
+        'ndc': '3253434553',
+        'prescriber': '0x89205A3A3b2A69De6Dbf7f01ED13B2108B2c43e7',
+        'date': 'November 18, 2020'
+    }
+]
+
+
+def home(request):
+    context = {
+        'prescriptions': prescriptions
+    }
+    return render(request, 'feed/home.html', context)
+
+
+def about(request):
+    return render(request, 'feed/about.html', {'title': 'About'})
