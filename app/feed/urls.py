f95ac19442db28bf906be2922ec57ccd4d40666e
austinpkugler <austinpkugler@gmail.com>
2021-06-21 09:32:20 -0700

    Add prescription form

--- app/feed/urls.py
+++ app/feed/urls.py
@@ -6,5 +6,6 @@ from . import forms
 
 urlpatterns = [
     path('', views.home, name='feed-home'),
-    path('about/', views.about, name='feed-about')
+    path('about/', views.about, name='feed-about'),
+    path('prescribe/', views.prescribe, name='feed-prescribe')
 ]
