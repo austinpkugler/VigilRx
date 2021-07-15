3ad2572dee21aab6f6196d11de5b8c5773430963
austinpkugler <austinpkugler@gmail.com>
2021-07-15 13:53:17 -0700

    Add Registrar and formatting

--- VigilRx/experiments/models.py
+++ VigilRx/experiments/models.py
@@ -3,43 +3,64 @@ import os
 
 from web3 import Web3
 
-import errors
-import compiled
-from compiled import w3
-from registrar import Registrar
+import bridge
+from bridge import w3
 
 
-registrar = Registrar()
+class Registrar():
+
+    def __init__(self):
+        self.personal_address = w3.eth.accounts[0]
+        self.sender = {'from': self.personal_address}
+
+        template = w3.eth.contract(abi=bridge.REGISTRAR_ABI, bytecode=bridge.REGISTRAR_BIN)
+        tx_hash = template.constructor().transact(self.sender)
+        tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
+
+        self.contract_address = tx_receipt.contractAddress
+        self.contract = w3.eth.contract(address=self.contract_address, abi=bridge.REGISTRAR_ABI)
+
+    def new_patient(self, personal_address):
+        tx_hash = self.contract.functions.createPatient(personal_address).transact(self.sender)
+        tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
+        tx_receipt = self.contract.events.NewAddress().processReceipt(tx_receipt)
+        return str(tx_receipt[0]['args']['contractAddress'])
+
+    def new_prescriber(self, personal_address, npi):
+        tx_hash = self.contract.functions.createPrescriber(
+            personal_address,
+            npi
+        ).transact(self.sender)
+        tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
+        tx_receipt = self.contract.events.NewAddress().processReceipt(tx_receipt)
+        return str(tx_receipt[0]['args']['contractAddress'])
+
+    def new_pharmacy(self, personal_address, npi):
+        tx_hash = self.contract.functions.createPharmacy(
+            personal_address,
+            npi
+        ).transact(self.sender)
+        tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
+        tx_receipt = self.contract.events.NewAddress().processReceipt(tx_receipt)
+        return str(tx_receipt[0]['args']['contractAddress'])
 
 
-class Prescription():
-
-    def __init__(self, contract_address, patient, ndc, quantity, refills):
-        self.contract_address = contract_address
-        self.patient = patient
-        self.ndc = ndc
-        self.quantity = quantity
-        self.refills = refills
-
-    def __repr__(self):
-        return f'Prescription(patient={self.patient}, ndc={self.ndc}, quantity={self.quantity}, refills={self.refills})'
-
-
-class Role():
+class Roles():
 
     def __init__(self, personal_address):
+        self.registrar = Registrar()
         self.personal_address = personal_address
-        self._from = {'from': self.personal_address}
+        self.sender = {'from': self.personal_address}
+        self.contract_address = None
+        self.contract = None
+        self.gas_used = 0
 
 
-class Provider(Role):
+class Provider(Roles):
 
     def __init__(self, npi, **kwargs):
         super().__init__(**kwargs)
         self.npi = npi
-        self.contract_address = None
-        self.contract = None
-        self._from = {'from': self.personal_address}
 
     def get_patients(self):
         patient_list = self.contract.functions.getPatientList().call()
@@ -53,39 +74,48 @@ class Provider(Role):
         return prescriptions
 
 
-class Patient(Role):
+class Patient(Roles):
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.contract_address = registrar.new_patient(self.personal_address)
-        self.contract = w3.eth.contract(address=self.contract_address, abi=compiled.PATIENT_ABI)
-        self.gas_used = 0
+        self.contract_address = self.registrar.new_patient(self.personal_address)
+        self.contract = w3.eth.contract(address=self.contract_address, abi=bridge.PATIENT_ABI)
 
     def __repr__(self):
-        return f'Patient(personal_address={self.personal_address}, contract_address={self.contract_address}, gas_used={self.gas_used})'
+        return f'Patient(contract_address={self.contract_address})'
 
     def add_permissioned(self, prescriber):
-        tx_hash = self.contract.functions.addPermissionedPrescriber(prescriber.contract_address).transact(self._from)
+        tx_hash = self.contract.functions.addPermissionedPrescriber(
+            prescriber.contract_address
+        ).transact(self.sender)
         tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
         self.gas_used += tx_receipt.gasUsed
 
     def remove_permissioned(self, prescriber):
-        tx_hash = self.contract.functions.removePermissionedPrescriber(prescriber.contract_address).transact(self._from)
+        tx_hash = self.contract.functions.removePermissionedPrescriber(
+            prescriber.contract_address
+        ).transact(self.sender)
         tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
         self.gas_used += tx_receipt.gasUsed
 
-    def add_prescription_permissions(self, prescription, pharmacy):
-        tx_hash = self.contract.functions.addPrescriptionPermissions(prescription.contract_address, pharmacy.contract_address).transact(self._from)
+    def add_prescription_permissions(self, prescription_address, pharmacy):
+        tx_hash = self.contract.functions.addPrescriptionPermissions(
+            prescription_address,
+            pharmacy.contract_address
+        ).transact(self.sender)
         tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
         self.gas_used += tx_receipt.gasUsed
 
-    def remove_prescription_permissions(self, prescription, pharmacy):
-        tx_hash = self.contract.functions.removePrescriptionPermissions(prescription.contract_address, pharmacy.contract_address).transact(self._from)
+    def remove_prescription_permissions(self, prescription_address, pharmacy):
+        tx_hash = self.contract.functions.removePrescriptionPermissions(
+            prescription_address,
+            pharmacy.contract_address
+        ).transact(self.sender)
         tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
         self.gas_used += tx_receipt.gasUsed
 
-    def request_fill(self, prescription):
-        tx_hash = self.contract.functions.requestFill(prescription.patient.contract_address).transact(self._from)
+    def request_fill(self, prescription_address):
+        tx_hash = self.contract.functions.requestFill(prescription_address).transact(self.sender)
         tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
         self.gas_used += tx_receipt.gasUsed
 
@@ -97,53 +127,65 @@ class Prescriber(Provider):
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.contract_address = registrar.new_prescriber(self.personal_address, self.npi)
-        self.contract = w3.eth.contract(address=self.contract_address, abi=compiled.PRESCRIBER_ABI)
-        self.gas_used = 0
+        self.contract_address = self.registrar.new_prescriber(self.personal_address, self.npi)
+        self.contract = w3.eth.contract(address=self.contract_address, abi=bridge.PRESCRIBER_ABI)
 
     def __repr__(self):
-        return f'Prescriber(personal_address={self.personal_address}, contract_address={self.contract_address}, gas_used={self.gas_used})'
+        return f'Prescriber(contract_address={self.contract_address})'
 
     def new_prescription(self, patient, ndc, quantity, refills):
-        tx_hash = self.contract.functions.createPrescription(patient.contract_address, ndc, quantity, refills).transact(self._from)
+        tx_hash = self.contract.functions.createPrescription(
+            patient.contract_address,
+            ndc,
+            quantity,
+            refills
+        ).transact(self.sender)
         tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
-        prescription_contract_address = str(self.contract.events.NewAddress().processReceipt(tx_receipt)[0]['args']['contractAddress'])
+        tx_receipt = self.contract.events.NewAddress().processReceipt(tx_receipt)
         self.gas_used += tx_receipt.gasUsed
-        return Prescription(prescription_contract_address, patient, ndc, quantity, refills)
+        return str(tx_receipt[0]['args']['contractAddress'])
 
-    def refill_prescription(self, prescription, refill_count):
-        tx_hash = self.contract.functions.refillPrescription(prescription.contract_address, refill_count).transact(self._from)
+    def refill_prescription(self, prescription_address, refill_count):
+        tx_hash = self.contract.functions.refillPrescription(
+            prescription_address,
+            refill_count
+        ).transact(self.sender)
         tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
-        prescription.refills = refill_count
+        prescription_address.refills = refill_count
         self.gas_used += tx_receipt.gasUsed
 
-    def cancel_prescription(self, prescription):
-        self.refill_prescription(prescription, 0)
-        prescription.refills = 0
+    def cancel_prescription(self, prescription_address):
+        self.refill_prescription(prescription_address, 0)
 
 
 class Pharmacy(Provider):
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.contract_address = registrar.new_pharmacy(self.personal_address, self.npi)
-        self.contract = w3.eth.contract(address=self.contract_address, abi=compiled.PHARMACY_ABI)
-        self.gas_used = 0
+        self.contract_address = self.registrar.new_pharmacy(self.personal_address, self.npi)
+        self.contract = w3.eth.contract(address=self.contract_address, abi=bridge.PHARMACY_ABI)
 
     def __repr__(self):
-        return f'Pharmacy(personal_address={self.personal_address}, contract_address={self.contract_address}, gas_used={self.gas_used})'
+        return f'Pharmacy(contract_address={self.contract_address})'
 
-    def add_prescription(self, prescription):
-        tx_hash = self.contract.functions.addPrescription(prescription.contract_address).transact(self._from)
+    def add_prescription(self, prescription_address):
+        tx_hash = self.contract.functions.addPrescription(
+            prescription_address
+        ).transact(self.sender)
         tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
         self.gas_used += tx_receipt.gasUsed
 
-    def fill(self, prescription, fill_count):
-        tx_hash = self.contract.functions.fillPrescription(prescription.contract_address, fill_count).transact(self._from)
+    def fill(self, prescription_address, fill_count):
+        tx_hash = self.contract.functions.fillPrescription(
+            prescription_address,
+            fill_count
+        ).transact(self.sender)
         tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
         self.gas_used += tx_receipt.gasUsed
 
-    def request_refill(self, prescription):
-        tx_hash = self.contract.functions.requestRefill(prescription.contract_address).transact(self._from)
+    def request_refill(self, prescription_address):
+        tx_hash = self.contract.functions.requestRefill(
+            prescription_address
+        ).transact(self.sender)
         tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
         self.gas_used += tx_receipt.gasUsed
